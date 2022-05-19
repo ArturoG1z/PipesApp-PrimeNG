8dfb43c373d5e4f8dca683420d82529c3dfdf211
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-18 23:47:25 -0500

    First progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pipesApp';
+  name: string = 'Arturo GOnzAlez';
+  value: number = 100100100100;
+  obj = {
+    brother: 'Juan',
+  }
+
+  constructor(private primeNGConfig: PrimeNGConfig) {
+  }
+
+  ngOnInit() {
+    this.primeNGConfig.ripple = true;
+  }
+
+  showName() {
+    console.log(this.name);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ButtonModule, RippleModule, CardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

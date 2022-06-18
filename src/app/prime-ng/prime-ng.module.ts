d5d4b7f651199d2d516d37d20ac32ae4dff4cc52
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-06-17 23:36:20 -0500

    Added some scroll dependencies for prime table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 import { FieldsetModule } from 'primeng/fieldset';
 import { InputTextModule } from 'primeng/inputtext';
 import { ListboxModule } from 'primeng/listbox';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
 import { RippleModule } from 'primeng/ripple';
-import { DividerModule } from 'primeng/divider';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+
 @NgModule({
   exports: [
     ButtonModule,
@@ -21,6 +24,8 @@ import { DividerModule } from 'primeng/divider';
     MenuModule,
     MenubarModule,
     RippleModule,
+    TableModule,
+    ToolbarModule,
   ],
 })
 export class PrimeNgModule {}
